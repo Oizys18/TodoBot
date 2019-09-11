a9264fd6f4c07aa2f8b76482a675f31be29b5008
Oizys18 <Oizys18@gmail.com>
2019-09-11 12:04:28 +0900

    replace

--- todos/urls.py
+++ todos/urls.py
@@ -3,11 +3,14 @@ from . import views
 
 app_name = 'todos'
 
+handler404 = 'my_app.views.handler404'
+handler500 = 'my_app.views.handler500'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('new/', views.new, name='new'),
+    # path('new/', views.new, name='new'),
     path('create/', views.create, name="create"),
-    path('edit/<int:pk>/', views.edit, name='edit'),
+    # path('edit/<int:pk>/', views.edit, name='edit'),
     path('update/<int:pk>/', views.update, name='update'),
     path('delete/<int:pk>/', views.delete, name='delete'),
 ]
